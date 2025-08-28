1ffad3662d2880ceb27aa242b456288453e65a8d
Yen Vy <vuthiyenvy000@gmail.com>
2025-08-28 13:52:46 +0700

    vd1

---
+++ vd_chuong1.py
@@ -0,0 +1,30 @@
+print("Viet ma chuong trinh")
+
+print("Albert Einstein da tung noi: Mot nguoi khong bao gio mac sai lam khong bao gio thu bat ky dieu gi moi.")
+
+if 5>2:
+    print("Five is greater than two")
+
+total = 1 + \
+2 + \
+3
+print(total); print("Hello python")
+
+if True:
+    print("True")
+else:
+    print("False")
+
+#This is a comment
+print("Hello, World!")
+
+import math
+import os
+
+import math_libs
+math_libs.add(4,5)
+
+def add(a,b):
+    result = a+b
+    return result
+
